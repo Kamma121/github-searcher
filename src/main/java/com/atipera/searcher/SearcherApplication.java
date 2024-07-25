fde4432459307267f4dddfb49edf6aaccf59ab50
Kamil <KamilSodo132@gmail.com>
2024-07-25 11:34:50 +0200

    Code review

--- src/main/java/com/atipera/searcher/SearcherApplication.java
+++ src/main/java/com/atipera/searcher/SearcherApplication.java
@@ -3,11 +3,20 @@ package com.atipera.searcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main application class for the Searcher application.
+ * This class serves as the entry point for the Spring Boot application.
+ */
 @SpringBootApplication
 public class SearcherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SearcherApplication.class, args);
-	}
+    /**
+     * The main method that starts the Spring Boot application.
+     *
+     * @param args The command line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SearcherApplication.class, args);
+    }
 
 }
